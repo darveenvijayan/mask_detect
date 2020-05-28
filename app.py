75fe7d3f6db26da0ab4a5c80d9970df538d45e0c
Darveen Vijayan <43519646+darveenvijayan@users.noreply.github.com>
2020-05-28 23:30:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -105,6 +105,8 @@ import dash_html_components as html
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
+app.title='3D Clusters'
 
 app.layout = html.Div(children=[
     html.H1(children='Hello Dash'),
